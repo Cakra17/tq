355ac53f704affb1870bbe922ce09c159aefd187
I Made Cakra Pustaka <imadecakra20@gmail.com>
2025-09-05 21:46:58 +0800

    feat: add new method for worker

--- internal/store/postgres.go
+++ internal/store/postgres.go
@@ -35,17 +35,17 @@ func (r *Repo) AddTask(ctx context.Context,t models.Task) error {
 	query := `
 		INSERT INTO tasks (
 			id, type, status, priority, user_id, resource_id, config,
-			started_at, completed_at, retry_count, max_retries,
+			created_at, started_at, completed_at, retry_count, max_retries,
 			assigned_worker_id, worked_assigned_at
 		) VALUES (
-			$1, $2, $3, $4, $5, $6, $7,
-			$8, $9, $10, $11, $12, $13
+			$1, $2, $3, $4, $5, $6, $7, 
+			$8, $9, $10, $11, $12, $13, $14
 		)
 	`
 
 	_, err = r.db.ExecContext(ctx, query, 
 		t.ID, t.Type, t.Status, t.Priority, t.UserID, t.ResourceID, t.Config,
-		t.StartedAt, t.CompletedAt, t.RetryCount, t.MaxRetries,
+		t.CreatedAt, t.StartedAt, t.CompletedAt, t.RetryCount, t.MaxRetries,
 		t.AssignedWorkerID, t.WorkedAssignedAt,
 	)
 	if err != nil {
@@ -64,6 +64,85 @@ func (r *Repo) AddTask(ctx context.Context,t models.Task) error {
 
 	return tx.Commit()
 }
+
+func (r *Repo) GetTask(ctx context.Context, id string) (*models.Task, error) {
+	var t models.Task
+	query := `
+	SELECT 
+		id, type, status, priority, user_id, resource_id, config,
+		created_at, started_at, completed_at, retry_count, max_retries,
+		assigned_worker_id, worker_assigned_at
+	FROM tasks WHERE id = $1`
+	
+	row := r.db.QueryRowContext(ctx, query, id)
+	err := row.Scan(
+		&t.ID, &t.Type, &t.Status, &t.Priority, &t.UserID, &t.ResourceID, &t.Config,
+		&t.CreatedAt, &t.StartedAt, &t.CompletedAt, &t.RetryCount, &t.MaxRetries,
+		&t.AssignedWorkerID, &t.WorkedAssignedAt,
+	)
+
+	if err == sql.ErrNoRows {
+		return nil, fmt.Errorf("Task not found\n")
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to get task: %v", err)
+	}
+
+	return &t, nil
+}
+
+func (r *Repo) PullNextTask(ctx context.Context, workerID string) (*models.Task, error) {
+	// Pop highest priority task (ZPOPMAX)
+	result, err := r.rd.ZPopMax(ctx, "task_queue", 1).Result()
+	if err == redis.Nil {
+		return nil, nil // no tasks available
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to pop task from queue: %v", err)
+	}
+
+	if len(result) == 0 {
+		return nil, nil
+	}
+
+	taskID := result[0].Member.(string)
+	task, err := r.GetTask(ctx, taskID)
+	if err != nil {
+		return nil, fmt.Errorf("task %s not found in database: %v", taskID, err)
+	}
+
+	now := time.Now()
+	task.Status = models.StatusRunning
+	task.StartedAt = &now
+	task.AssignedWorkerID = &workerID
+	task.WorkedAssignedAt = &now
+
+	err = r.UpdateTaskStatus(ctx, taskID, task.Status, *task.AssignedWorkerID, task.StartedAt, task.WorkedAssignedAt)
+	if err != nil {
+		r.rd.ZAdd(ctx, "task_queue", redis.Z{
+			Score: result[0].Score,
+			Member: taskID,
+		})
+		return nil, fmt.Errorf("Failed to update task status: %v", err)
+	}
+	return task, nil
+}
+
+func (r *Repo) UpdateTaskStatus(
+	ctx context.Context, taskID, status, workerID string, 
+	startedAt, workedAssignAt *time.Time,
+) error {
+	query := `
+		UPDATE tasks
+		SET status = $1, assigned_worker_id = $2, worker_assigned_at = $3
+		WHERE id = $4
+	`
+	_, err := r.db.ExecContext(ctx, query, status, workerID, workedAssignAt, taskID)
+	return err
+}
+
 
 func ConnectDB(cdf config.DatabaseConfig) *sql.DB {
 	dsn := fmt.Sprintf(
