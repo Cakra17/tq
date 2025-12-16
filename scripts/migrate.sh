eabd04b2b328ea577973258c402ec6956609b339
I Made Cakra Pustaka <imadecakra20@gmail.com>
2025-12-16 21:55:25 +0800

    update script & table

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,1 +1,11 @@
-migrate -verbose -path=./db/migrations -database postgres://admin:adminsecret@localhost:5432/tq?sslmode=disable up
+#!/usr/bin/env bash
+
+case "$1" in
+  up | down)
+    migrate -verbose -path=./db/migrations -database postgres://admin:adminsecret@localhost:5432/tq?sslmode=disable $1
+    ;;
+  *)
+    echo "you have to pass 'up' or 'down' as an argument"
+    exit 1
+    ;;
+esac
